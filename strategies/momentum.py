116de37e81e2461809340d894dd31dd2318907dd
gandiv-star <gandivacademy@gmail.com>
2026-09-14 13:30:21 +0530

    Create momentum.py

---
+++ strategies/momentum.py
@@ -0,0 +1,14 @@
+from core.constants import Decision
+from features.feature_engine import FeatureSet
+from regime.regime_engine import RegimeAssessment
+from strategies.base_strategy import BaseStrategy, StrategySignal
+
+
+class MomentumStrategy(BaseStrategy):
+    name = "momentum"
+    version = "0.1.0"
+
+    def evaluate(
+        self, symbol: str, features: FeatureSet, regime: RegimeAssessment
+    ) -> StrategySignal:
+        raise NotImplementedError
